bb14d13f57ffe172714dc314a0bdadc4e6cd8efb
Chlodion Gossart <chlodiongossart@gmail.com>
2022-04-12 18:33:04 +0200

    added mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,19 @@
+//Import d'Express
 const express = require('express');
 
+//Utilisation d'Express
 const app = express();
 
+//Import de mongoDB
+const mongoose = require('mongoose');
+
+//Mise en place de la base de données mongoDB
+mongoose.connect('mongodb+srv://Riocamy:p6-piiquante@piiquanteserver.5petj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
+//Export et exploitation de l'API
 module.exports = app;
